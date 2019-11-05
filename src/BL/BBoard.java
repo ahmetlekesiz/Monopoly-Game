833edd6570ef97c9ab7a9640f42f79ebe3696cbb
Muhammed Bera Koç <z.bera97@gmail.com>
2019-11-06 02:49:29 +0300

    Board class implemented.

--- src/BL/BBoard.java
+++ src/BL/BBoard.java
@@ -1,4 +1,21 @@
 package BL;
 
+import DAL.Instruction;
+
 public class BBoard {
+
+    private static final int SQUARE_NUMBER = 40;
+    private BSquare[] squares;
+    private static BBoard boardInstance;
+    private Instruction instructionInstance;
+    private int turnCounter;
+
+    private BBoard() {
+        instructionInstance = Instruction.getInstance();
+        squares = new BSquare[SQUARE_NUMBER];
+    }
+
+    public static BBoard getInstance() {
+        return boardInstance;
+    }
 }
