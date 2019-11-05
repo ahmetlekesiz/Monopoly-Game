c558f1e13664bed4768d11e08a2be2d4d85244d7
salih <salih.zyurt@gmail.com>
2019-11-05 22:26:21 +0300

    TerminalOutput class is modified.

--- src/view/terminal/TerminalOutput.java
+++ src/view/terminal/TerminalOutput.java
@@ -1,4 +1,14 @@
 package view.terminal;
 
+import model.Instruction;
+import model.JSONReader;
+
 public class TerminalOutput {
+    private Instruction currentInstruction;
+
+    private TerminalOutput() {
+        JSONReader objJsonReader = new JSONReader("game.json");
+        currentInstruction = Instruction.getInstance();
+    }
+
 }
