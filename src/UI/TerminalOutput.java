c80988e4d74a53ce449fe5c8dd38c53ea8d09248
Muhammed Bera Koç <z.bera97@gmail.com>
2019-11-06 01:51:07 +0300

    BTaxSquare implemented.

--- src/UI/TerminalOutput.java
+++ src/UI/TerminalOutput.java
@@ -1,23 +1,14 @@
 package UI;
 
-import DAL.Instruction;
-import DAL.JSONReader;
 
 public class TerminalOutput {
-    private Instruction currentInstruction;
 
     private TerminalOutput(String JSONFileName) {
-        JSONReader objJsonReader = new JSONReader(JSONFileName);
-        objJsonReader.initInstructionObject();
-        currentInstruction = Instruction.getInstance();
+
     }
 
     public static TerminalOutput createTerminalOutput(String JSONFileName) {
         return new TerminalOutput(JSONFileName);
-    }
-
-    public void printTerminalOutput() {
-        System.out.println(currentInstruction.toString());
     }
 
 }
