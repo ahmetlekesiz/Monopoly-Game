0e36c3f82d43d70ca0faa8f7981e287e66dd3d8c
mucahittanacioglu <mcht_bf@outlook.com>
2019-11-06 02:02:30 +0300

    BPlayer

--- src/BL/BPlayer.java
+++ src/BL/BPlayer.java
@@ -4,8 +4,56 @@ import DAL.Piece;
 
 public class BPlayer {
 
-    int balance, cycleCounter, totalDiceValue;
-    boolean bankruptFlag;
-    Piece.PIECE_TYPE piece_type;
+    private int balance, cycleCounter, totalDiceValue;
+    private boolean bankruptFlag;
+    private Piece.PIECE_TYPE piece_type;
+
+    public BPlayer(int balance, int cycleCounter, int totalDiceValue, boolean bankruptFlag, Piece.PIECE_TYPE piece_type) {
+        this.balance = balance;
+        this.cycleCounter = cycleCounter;
+        this.totalDiceValue = totalDiceValue;
+        this.bankruptFlag = bankruptFlag;
+        this.piece_type = piece_type;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public int getCycleCounter() {
+        return cycleCounter;
+    }
+
+    public void setCycleCounter(int cycleCounter) {
+        this.cycleCounter = cycleCounter;
+    }
+
+    public int getTotalDiceValue() {
+        return totalDiceValue;
+    }
+
+    public void setTotalDiceValue(int totalDiceValue) {
+        this.totalDiceValue = totalDiceValue;
+    }
+
+    public boolean isBankruptFlag() {
+        return bankruptFlag;
+    }
+
+    public void setBankruptFlag(boolean bankruptFlag) {
+        this.bankruptFlag = bankruptFlag;
+    }
+
+    public Piece.PIECE_TYPE getPiece_type() {
+        return piece_type;
+    }
+
+    public void setPiece_type(Piece.PIECE_TYPE piece_type) {
+        this.piece_type = piece_type;
+    }
 
 }
