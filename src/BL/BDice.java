7ecc5c75dc60b414220c644c96cfeaebe1b4cec0
Muhammed Bera Koç <z.bera97@gmail.com>
2019-11-06 02:18:46 +0300

    f001 branch created.

--- src/BL/BDice.java
+++ src/BL/BDice.java
@@ -2,7 +2,7 @@ package BL;
 
 public class BDice {
     public int[] rollDice(){
-        int diceArray[] = new int[2];
+        int[] diceArray = new int[2];
         double randomDouble = (Math.random() * 6) + 1;
         diceArray[0] = (int) randomDouble;
         randomDouble = (Math.random() * 6) + 1;
