5fde4a9d2ca4d2fcaf2f4da4b32dbedaf158df84
Muhammed Bera Koç <z.bera97@gmail.com>
2019-11-06 23:41:12 +0300

    Project modified and optimised.

--- src/BL/BSquare.java
+++ src/BL/BSquare.java
@@ -3,23 +3,15 @@ package BL;
 import DAL.DPlayer;
 
 public class BSquare {
-    private int Id;
+
     private final String SQUARE_TYPE = "NONE";
 
     public void performOnLand(DPlayer player){
-        player.setLocation(this.Id);
+
     };
 
     public String getSQUARE_TYPE() {
         return SQUARE_TYPE;
-    }
-
-    public int getId() {
-        return Id;
-    }
-
-    public void setId(int id) {
-        Id = id;
     }
 
     public BSquare(){
--- src/BL/BTaxSquare.java
+++ src/BL/BTaxSquare.java
@@ -8,12 +8,8 @@ public class BTaxSquare extends BSquare {
     private final String SQUARE_TYPE = "TAX_SQUARE";
     private final DInstruction DInstructionInstance = DInstruction.getInstance();
 
-    public BTaxSquare(int id){
-        this.setId(id);
-    }
     @Override
     public void performOnLand(DPlayer player) {
-        player.setLocation(this.getId());
         player.setBalance(player.getBalance() - (int) DInstructionInstance.priceOfTaxSquares);
     }
 }
