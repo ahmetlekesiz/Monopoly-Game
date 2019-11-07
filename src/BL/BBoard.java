c3aebe75765cec7de53fd06f1e50aaa0fb3a12af
ahmetlekesiz <ahmetlekesiz@gmail.com>
2019-11-07 16:12:59 +0300

    BBoard - TaxSquareLocation set to random values.

--- src/BL/BBoard.java
+++ src/BL/BBoard.java
@@ -3,6 +3,8 @@ package BL;
 import DAL.DInstruction;
 import com.sun.org.apache.bcel.internal.generic.Instruction;
 
+import java.util.ArrayList;
+
 public class BBoard {
 
     private static final int SQUARE_NUMBER = 40;
@@ -17,23 +19,33 @@ public class BBoard {
         countOfTaxSquares = (int) instructionInstance.countOfTaxSquares;
         initSquares();
     }
-
+    /**
+     *<p>Create an ArrayList of Squares.
+     *Generate random location of taxSquares and declare the ArrayList.</p>
+     *
+     * @return void
+     */
     private void initSquares() {
-        int[] taxSquareLocations = new int[countOfTaxSquares];
+        ArrayList<Integer> taxSquareLocations = new ArrayList<>();
         int temp;
-        // TODO May return same number optimise
         for(int i = 0; i < countOfTaxSquares; i++){
             temp = (int) (Math.random() * SQUARE_NUMBER);
-            taxSquareLocations[i] = temp;
+            while(!taxSquareLocations.contains(temp)){
+                temp = (int) (Math.random() * SQUARE_NUMBER);
+            }
+            taxSquareLocations.add(temp);
         }
+
         for (int i = 0; i < SQUARE_NUMBER; ++i) {
             squares[i] = new BRegularSquare();
         }
         for(int i = 0; i < countOfTaxSquares; i++) {
-            squares[taxSquareLocations[i]] = new BTaxSquare();
+            squares[taxSquareLocations.get(i)] = new BTaxSquare();
         }
         squares[0] = new BGoSquare();
     }
+
+
 
     public BSquare[] getSquares() {
         return squares;
