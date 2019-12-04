c26f2664350fd7a5eb4f6cdbedd6b4102ce7c4ac
Muhammed Bera Koç <z.bera97@gmail.com>
2019-12-04 20:55:50 +0300

    Added test and JDoc for BJailSquare class

--- src/BL/BJailSquare.java
+++ src/BL/BJailSquare.java
@@ -1,25 +1,43 @@
 package BL;
 
-
+import DAL.DInstruction;
 import DAL.DPlayer;
 
 import java.util.HashMap;
 
-public class BJailSquare extends BSquare{
+/**
+ * <h>BJailSquare</h>
+ *
+ * <p>A class for Jail Component in Monopoly game.</p>
+ *
+ * @author Muhammed Bera Koç
+ * @version 1.1
+ */
+public class BJailSquare extends BSquare {
 
     private HashMap<DPlayer, Integer> jailRecords;
-    private final int JAIL_PRICE = 25;
 
     public BJailSquare() {
         jailRecords = new HashMap<>();
     }
 
+    /**
+     *
+     * <p>When player reaches the Jail square this method will be triggered. It adds player to jailRecords
+     * and marks player as arrested.</p>
+     *
+     * @param player
+     * @return void
+     */
     @Override
     public void performOnLand(DPlayer player) {
         jailRecords.put(player, 0);
         player.setArrested(true);
     }
 
+    /**
+     * <p>In each tour scans the jail records to conduct appropriate actions for those who are arrested.</p>
+     */
     public void scanJailRecords() {
         for (DPlayer currentPlayer: jailRecords.keySet()) {
             int stayCount = jailRecords.get(currentPlayer);
@@ -27,11 +45,14 @@ public class BJailSquare extends BSquare{
                 int[] currentDiceValues = currentPlayer.getPlayerDice().rollDice();
                 if (currentDiceValues[0] == currentDiceValues[1]) {
                     currentPlayer.setArrested(false);
+                    jailRecords.remove(currentPlayer);
+                    return;
                 }
                 jailRecords.put(currentPlayer, ++stayCount);
             } else {
-                currentPlayer.setBalance(currentPlayer.getBalance() - JAIL_PRICE);
+                currentPlayer.setBalance(currentPlayer.getBalance() - (int) DInstruction.getInstance().priceOfJailSquare);
                 currentPlayer.setArrested(false);
+                jailRecords.remove(currentPlayer);
             }
         }
     }
@@ -39,5 +60,9 @@ public class BJailSquare extends BSquare{
     @Override
     public String getSQUARE_TYPE() {
         return null;
+    }
+
+    public HashMap<DPlayer, Integer> getJailRecords() {
+        return jailRecords;
     }
 }
--- tests/DAL/DInstructionTest.java
+++ tests/DAL/DInstructionTest.java
@@ -9,9 +9,9 @@ class DInstructionTest {
     @Test
     void testToString() {
         DInstruction dInstruction = DInstruction.getInstance(2,
-                1000, 4, 40, 50);
+                1000, 4, 40, 50, 25);
         String string = "countOfPlayers: 2\nstartMoney: 1000\ncountOfTaxSquares: 4\npriceOfTaxSquares: 40\n" +
-                "priceOfGoSquare: 50";
+                "priceOfGoSquare: 50\npriceOfJailSquare: 25";
         assertEquals(string, dInstruction.toString());
     }
 }
